323a2715ad63a6ab2d22f99a75959be2e4767d08
Isklar <mrisklar@gmail.com>
2016-01-27 17:42:38 +0000

    Added voice channel checkers to music related commands

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -502,36 +502,47 @@ class MusicBot(discord.Client):
         if player.is_stopped:
             player.play()
 
-    async def handle_pause(self, player):
+    async def handle_pause(self, player, channel, author):
         """
         Usage {command_prefix}pause
         Pauses playback of the current song. [todo: should make sure it works fine when used inbetween songs]
         """
+        voice_client = self.voice_clients[channel.server.id]
+        if discord.utils.get(voice_client.channel.voice_members, id=author.id):
+            if player.is_playing:
+                player.pause()
 
-        if player.is_playing:
-            player.pause()
-
+            else:
+                raise CommandError('Player is not playing.')
         else:
-            raise CommandError('Player is not playing.')
+           raise CommandError('This bot isnt in your channel %s!' % author) 
 
-    async def handle_resume(self, player):
+    async def handle_resume(self, player, channel, author):
         """
         Usage {command_prefix}resume
         Resumes playback of a paused song.
         """
-        if player.is_paused:
-            player.resume()
+        voice_client = self.voice_clients[channel.server.id]
+        if discord.utils.get(voice_client.channel.voice_members, id=author.id):
+            if player.is_paused:
+                player.resume()
 
+            else:
+                raise CommandError('Player is not paused.')
         else:
-            raise CommandError('Player is not paused.')
+           raise CommandError('This bot isnt in your channel %s!' % author)
 
-    async def handle_shuffle(self, player):
+    async def handle_shuffle(self, player, channel, author):
         """
         Usage {command_prefix}shuffle
         Shuffles the playlist.
         """
-        player.playlist.shuffle()
-        return Response('*shuffleshuffleshuffle*', delete_after=10)
+        voice_client = self.voice_clients[channel.server.id]
+        if discord.utils.get(voice_client.channel.voice_members, id=author.id):
+            player.playlist.shuffle()
+            return Response('*shuffleshuffleshuffle*', delete_after=10)
+        else:
+           raise CommandError('This bot isnt in your channel %s!' % author)
 
     async def handle_clear(self, player, author):
         """
@@ -547,89 +558,95 @@ class MusicBot(discord.Client):
         Usage {command_prefix}skip
         Skips the current song when enough votes are cast, or by the bot owner.
         """
+        voice_client = self.voice_clients[channel.server.id]
+        if discord.utils.get(voice_client.channel.voice_members, id=author.id):
+            if player.is_stopped or player.is_paused: # TODO: pausing and skipping a song breaks /something/, i'm not sure what
+                raise CommandError("Can't skip! The player is not playing!")
 
-        if player.is_stopped or player.is_paused: # TODO: pausing and skipping a song breaks /something/, i'm not sure what
-            raise CommandError("Can't skip! The player is not playing!")
+            if author.id == self.config.owner_id:
+                player.skip()
+                return
 
-        if author.id == self.config.owner_id:
-            player.skip()
-            return
+            voice_channel = player.voice_client.channel
 
-        voice_channel = player.voice_client.channel
+            num_voice = sum(1 for m in voice_channel.voice_members if not (
+                m.deaf or m.self_deaf or m.id == str(self.config.owner_id)))
 
-        num_voice = sum(1 for m in voice_channel.voice_members if not (
-            m.deaf or m.self_deaf or m.id == str(self.config.owner_id)))
+            num_skips = player.skip_state.add_skipper(author.id)
 
-        num_skips = player.skip_state.add_skipper(author.id)
+            skips_remaining = min(self.config.skips_required, round(num_voice * self.config.skip_ratio_required)) - num_skips
 
-        skips_remaining = min(self.config.skips_required, round(num_voice * self.config.skip_ratio_required)) - num_skips
+            if skips_remaining <= 0:
+                player.skip()
+                return Response(
+                    'your skip for **{}** was acknowledged.'
+                    '\nThe vote to skip has been passed.{}'.format(
+                        player.current_entry.title,
+                        ' Next song coming up!' if player.playlist.peek() else ''
+                    ),
+                    reply=True,
+                    delete_after=10
+                )
 
-        if skips_remaining <= 0:
-            player.skip()
-            return Response(
-                'your skip for **{}** was acknowledged.'
-                '\nThe vote to skip has been passed.{}'.format(
-                    player.current_entry.title,
-                    ' Next song coming up!' if player.playlist.peek() else ''
-                ),
-                reply=True,
-                delete_after=10
-            )
-
+            else:
+                # TODO: When a song gets skipped, delete the old x needed to skip messages
+                return Response(
+                    'your skip for **{}** was acknowledged.'
+                    '\n**{}** more {} required to vote to skip this song.'.format(
+                        player.current_entry.title,
+                        skips_remaining,
+                        'person is' if skips_remaining == 1 else 'people are'
+                    ),
+                    reply=True
+                )
         else:
-            # TODO: When a song gets skipped, delete the old x needed to skip messages
-            return Response(
-                'your skip for **{}** was acknowledged.'
-                '\n**{}** more {} required to vote to skip this song.'.format(
-                    player.current_entry.title,
-                    skips_remaining,
-                    'person is' if skips_remaining == 1 else 'people are'
-                ),
-                reply=True
-            )
-
-    async def handle_volume(self, message, new_volume=None):
+           raise CommandError('This bot isnt in your channel %s!' % author)
+           
+    async def handle_volume(self, message, channel, author, new_volume=None):
         """
         Usage {command_prefix}volume (+/-)[volume]
         Sets the playback volume. Accepted values are from 1 to 100.
         Putting + or - before the volume will make the volume change relative to the current volume.
         """
+        voice_client = self.voice_clients[channel.server.id]
+        if discord.utils.get(voice_client.channel.voice_members, id=author.id):
+            player = await self.get_player(message.channel)
 
-        player = await self.get_player(message.channel)
+            if not new_volume:
+                return Response('Current volume: `%s%%`' % int(player.volume * 100), reply=True, delete_after=10)
 
-        if not new_volume:
-            return Response('Current volume: `%s%%`' % int(player.volume * 100), reply=True, delete_after=10)
+            relative = False
+            if new_volume[0] in '+-':
+                relative = True
 
-        relative = False
-        if new_volume[0] in '+-':
-            relative = True
+            try:
+                new_volume = int(new_volume)
 
-        try:
-            new_volume = int(new_volume)
+            except ValueError:
+                raise CommandError('{} is not a valid number'.format(new_volume))
 
-        except ValueError:
-            raise CommandError('{} is not a valid number'.format(new_volume))
-
-        if relative:
-            vol_change = new_volume
-            new_volume += (player.volume * 100)
-
-        old_volume = int(player.volume * 100)
-
-        if 0 < new_volume <= 100:
-            player.volume = new_volume / 100.0
-
-            return Response('updated volume from %d to %d' % (old_volume, new_volume), reply=True, delete_after=10)
-
-        else:
             if relative:
-                raise CommandError(
-                    'Unreasonable volume change provided: {}{:+} -> {}%.  Provide a change between {} and {:+}.'.format(
-                        old_volume, vol_change, old_volume + vol_change, 1 - old_volume, 100 - old_volume))
-            else:
-                raise CommandError(
-                    'Unreasonable volume provided: {}%. Provide a value between 1 and 100.'.format(new_volume))
+                vol_change = new_volume
+                new_volume += (player.volume * 100)
 
+            old_volume = int(player.volume * 100)
+
+            if 0 < new_volume <= 100:
+                player.volume = new_volume / 100.0
+
+                return Response('updated volume from %d to %d' % (old_volume, new_volume), reply=True, delete_after=10)
+
+            else:
+                if relative:
+                    raise CommandError(
+                        'Unreasonable volume change provided: {}{:+} -> {}%.  Provide a change between {} and {:+}.'.format(
+                            old_volume, vol_change, old_volume + vol_change, 1 - old_volume, 100 - old_volume))
+                else:
+                    raise CommandError(
+                        'Unreasonable volume provided: {}%. Provide a value between 1 and 100.'.format(new_volume))
+        else:
+           raise CommandError('This bot isnt in your channel %s!' % author)
+           
     async def handle_queue(self, channel):
         """
         Usage {command_prefix}queue
